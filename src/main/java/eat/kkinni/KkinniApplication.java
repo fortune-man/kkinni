8caf6a0f221ae42c21bbf0e6e00cfaa973c5b817
fortune-man <wnguddl96@knou.ac.kr>
2025-01-22 18:46:37 +0900

    Initial commit

---
+++ src/main/java/eat/kkinni/KkinniApplication.java
@@ -0,0 +1,13 @@
+package eat.kkinni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkinniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkinniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eat/kkinni/KkinniApplicationTests.java
@@ -0,0 +1,13 @@
+package eat.kkinni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkinniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
