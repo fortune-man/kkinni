fa7658cad6ac138e1d8f74934516fa57365100eb
fortune-man <wnguddl96@knou.ac.kr>
2025-02-22 20:01:59 +0900

    test : 의미없는 커밋 5 개발에 흥미 붙이기

--- src/test/java/eat/kkinni/config/RedisConfigTest.java
+++ src/test/java/eat/kkinni/config/RedisConfigTest.java
@@ -5,8 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class RedisConfigTest {
-
+  final RedisConfig redisConfig = new RedisConfig();
   void codeForFun() {
+    String string = redisConfig.toString();
+    System.out.println(string);
 
   }
 }
