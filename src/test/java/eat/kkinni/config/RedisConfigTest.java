40521a2c719b3698c9a82c6d4b226ce2e072cf42
fortune-man <wnguddl96@knou.ac.kr>
2025-02-22 19:56:27 +0900

    test : 의미없는 커밋

--- src/test/java/eat/kkinni/config/RedisConfigTest.java
+++ src/test/java/eat/kkinni/config/RedisConfigTest.java
@@ -5,5 +5,5 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class RedisConfigTest {
-  // 목표없이 최대한 많이 최대한 명확하게 커밋
+  final int REDIS = 0;
 }
